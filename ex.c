2a4288fccd1c014473eebe795c346e9e8039e2e6
neha46 <109017609+neha46@users.noreply.github.com>
2022-07-21 01:05:45 +0530

    1 assignment

---
+++ ex.c
@@ -0,0 +1,7 @@
+#include<stdio.h>// error
+int main()
+{
+int x = printf(“ineuron”);
+printf(“%d”,x);
+return 0;
+}
---
+++ strlen.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=printf("neha");// or we canuse  sizeof operator
+    printf(" length is %d",a);
+    return 0;
+}
