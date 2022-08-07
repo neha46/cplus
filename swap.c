b57adbd950ca6befc844053f61cc22637b153837
neha46 <109017609+neha46@users.noreply.github.com>
2022-08-07 17:01:31 +0530

    swap.cpp

--- swap.c
+++ swap.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
     int num1,num2,t;
-    printf("enter  number\n");
-    scanf("%d%d",&num1,&num2);
+    cout<<"enter  number\n";
+    cin>>num1>>num2;
     //before swaping
-    printf("before swapping\n");
-    printf("num1=%d\t num2=%d\n",num1,num2);
+    cout<<"before swapping\n";
+    cout<<"num1=" <<num1<<" num2= "<<num2<<endl;
     t=num1;
     num1=num2;
     num2=t;
     //after swaping
-    printf("after swapping\n");
-    printf("num1=%d\t num2=%d\n",num1,num2);
+    cout<<"after swapping\n";
+        cout<<"num1=" <<num1<<" num2= "<<num2;
     return 0;
 
 }
