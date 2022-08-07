736f1f7b4f5b184185893b8a65da17038a311d52
neha46 <109017609+neha46@users.noreply.github.com>
2022-08-07 16:50:06 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
     int r;
     float a;
-    printf("enter radius for area of circle\n");
-    scanf(" %d",&r);
+    cout<<"enter radius for area of circle\n";
+    cin>>r;
     a=(2*r*3.14);
-    printf("Area of circle is %f  having the radius %d",a,r);
+    cout<<"Area of circle is "<<a<<"  having the radius "<<r;
     return 0;
 }
+
