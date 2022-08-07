838eb3d57f1b6314fdb0ac0b2aea978aec2707cd
neha46 <109017609+neha46@users.noreply.github.com>
2022-08-07 17:25:48 +0530

    Update lastDigit0.c

--- lastDigit0.c
+++ lastDigit0.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
     int num,a;
     int div=10;
     int digit=-1;
-    printf("Enter the number\n");
-    scanf("%d",&num);
+    cout<<"Enter the number\n";
+    cin>>num;
 a=num/10;
-printf("Numer with last digit as 0 is %d\n",a*10);
+cout<<"Numer with last digit as 0 is "<<a*10;
 return 0;
 }
