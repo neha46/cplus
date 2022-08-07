a36a6fedb6184fc08933dd91bf73d4c391d91524
neha46 <109017609+neha46@users.noreply.github.com>
2022-08-07 16:25:40 +0530

    Update INR.C

--- INR.C
+++ INR.C
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
     float  one_usd=76.23,rs;
     long int am;
-    printf("Enter amount\n");
-    scanf("%d",&am);
+    cout<<"Enter amount\n"<<endl;
+    cin>>am;
     rs=1/one_usd;
-    printf("USD is %d\n",am/rs);
+    cout<<"USD is"<<(am/rs);
     return 0;
 
 }
