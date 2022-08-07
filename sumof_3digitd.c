609dd76e4f1d226db0a201685b4b2c84461cce02
neha46 <109017609+neha46@users.noreply.github.com>
2022-08-07 17:05:11 +0530

    Update sumof_3digitd.c

--- sumof_3digitd.c
+++ sumof_3digitd.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
     int n,sum=0;
-    printf("enter  number\n");
-    scanf("%d",&n);
-    printf("number is =%d\n",n);
+    cout<<"enter  number\n";
+   cin>>n;
 while(n!=0)
     {
         sum=sum+n%10;
         n=n/10;
 
     }
-    printf("sum is %d",sum);
+    cout<<"sum is "<<sum;
     return 0;
 
 }
